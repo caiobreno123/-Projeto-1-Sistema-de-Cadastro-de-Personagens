5ca85ac21a0580127893bde413cd914dbe5c8306
caiobreno123 <caiobrenopiresduarte@gmail.com>
2025-08-19 21:16:57 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const Personagem = require('./class/Personagem.js')
+
+const personagem1 = new Personagem('Bebo Véi', 'Guerreiro', 120)
+const personagem2 = new Personagem('Guilas', 'Mago', 70)
+const personagem3 = new Personagem('Botelho', 'Arqueiro', 95)
+
+console.log("==================")
+personagem1.mostrarInfo()
+console.log("==================")
+personagem1.atacar()
+console.log("==================")
+personagem2.mostrarInfo()
+console.log("==================")
+personagem2.atacar()
+console.log("==================")
+personagem3.mostrarInfo()
+console.log("==================")
+personagem3.atacar()
