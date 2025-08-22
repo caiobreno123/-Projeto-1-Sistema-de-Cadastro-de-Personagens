4d643247ffd3358223d85d6367e359bc0556af03
caiobreno123 <caiobrenopiresduarte@gmail.com>
2025-08-22 12:58:31 -0300

    Add files via upload

---
+++ src/Personagem.js
@@ -0,0 +1,32 @@
+class Personagem {
+    
+    #vida
+    constructor(nome, classe, vida) {
+        this.nome = nome
+        this.classe = classe
+        this.#vida = vida
+    }
+    atacar() {
+        switch (this.classe) {
+            case 'Guerreiro':
+                console.log(`${this.nome} desferiu um golpe poderoso com sua espada!`)
+                break
+            case 'Mago':
+                console.log(`${this.nome} lançou uma magia de fogo!`)
+                break
+            case 'Arqueiro':
+                console.log(`${this.nome} atirou uma flecha precisa!`)
+                break
+            default:
+                console.log(`${this.nome} atacou!`)
+        }
+    }
+    mostrarInfo() {
+        console.log(`📋 Nome: ${this.nome}`)
+        console.log(`🏹 Classe: ${this.classe}`)
+        console.log(`❤️ Vida: ${this.#vida}`)
+    }
+}
+
+
+module.exports = Personagem
