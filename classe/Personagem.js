8d85840f7c6044bb2e92a6095abe6f807828d80b
caiobreno123 <caiobrenopiresduarte@gmail.com>
2025-08-22 08:48:36 -0300

    Update Personagem.js

--- classe/Personagem.js
+++ classe/Personagem.js
@@ -1,32 +1,32 @@
 class Personagem {
-    #nome
-    #classe
+    
     #vida
     constructor(nome, classe, vida) {
-        this.#nome = nome
-        this.#classe = classe
+        this.nome = nome
+        this.classe = classe
         this.#vida = vida
     }
     atacar() {
         switch (this.#classe) {
             case 'Guerreiro':
-                console.log(`${this.#nome} desferiu um golpe poderoso com sua espada!`)
+                console.log(`${this.nome} desferiu um golpe poderoso com sua espada!`)
                 break
             case 'Mago':
-                console.log(`${this.#nome} lançou uma magia de fogo!`)
+                console.log(`${this.nome} lançou uma magia de fogo!`)
                 break
             case 'Arqueiro':
-                console.log(`${this.#nome} atirou uma flecha precisa!`)
+                console.log(`${this.nome} atirou uma flecha precisa!`)
                 break
             default:
-                console.log(`${this.#nome} atacou!`)
+                console.log(`${this.nome} atacou!`)
         }
     }
     mostrarInfo() {
-        console.log(`📋 Nome: ${this.#nome}`)
-        console.log(`🏹 Classe: ${this.#classe}`)
+        console.log(`📋 Nome: ${this.nome}`)
+        console.log(`🏹 Classe: ${this.classe}`)
         console.log(`❤️ Vida: ${this.#vida}`)
     }
 }
+
 
 module.exports = Personagem
